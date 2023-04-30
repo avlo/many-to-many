9486129cb04287e3365787c34ab1144677c8d395
Avlonitis <Avlonitis.Nick@mayo.edu>
2023-04-30 01:13:27 -0500

    updated escalationrequest successful mapping

--- src/main/java/com/example/demo/entity/EscalationRequest.java
+++ src/main/java/com/example/demo/entity/EscalationRequest.java
@@ -1,15 +1,10 @@
 package com.example.demo.entity;
 
 import java.io.Serializable;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
-import javax.persistence.OneToOne;
 import lombok.Data;
 
 @Data
