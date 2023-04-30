eb262bb6ec0847a36e57ca8778e15b79df4a4986
Avlonitis <Avlonitis.Nick@mayo.edu>
2023-04-30 00:16:35 -0500

    updated escalationrequest successful mapping

--- src/test/java/com/example/demo/EscalationServiceTest.java
+++ src/test/java/com/example/demo/EscalationServiceTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EscalationServiceTest {
+public class EscalationServiceTest {
   @Autowired EscalationService escalationService;
 
   @Test
