5d7e57e0b90b1d1e7c1c3472dbd762f64da62911
Avlonitis <Avlonitis.Nick@mayo.edu>
2023-04-30 22:18:24 -0500

    working integration tests, but doesnt appear to be on sqlserver

--- src/main/java/com/example/demo/service/EscalationRequestService.java
+++ src/main/java/com/example/demo/service/EscalationRequestService.java
@@ -4,9 +4,12 @@ import com.example.demo.entity.Escalation;
 import com.example.demo.entity.EscalationRequest;
 import com.example.demo.entity.Request;
 import com.example.demo.repository.EscalationRequestRepository;
+//import com.google.common.base.Preconditions;
+import com.sun.istack.NotNull;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,7 +37,7 @@ public class EscalationRequestService {
   }
 
   public Collection<Request> getEscalationRequests(Integer escalationId) {
-    Optional<Collection<EscalationRequest>> escalations = getEscalationRequestRecords(111);
+    Optional<Collection<EscalationRequest>> escalations = getEscalationRequestRecords(escalationId);
 
     // TODO: refactor all below lines to more efficient retrieval
     Collection<Request> requests = new ArrayList<>();
@@ -43,5 +46,24 @@ public class EscalationRequestService {
       requests.add(requestService.findByRequestId(escalationRequest.getRequestId()).get());
     }
     return requests;
+  }
+
+  @Transactional
+  public void save(@NotNull Escalation escalation, @NotNull Collection<Request> requests) {
+    for (Request request : requests) {
+        save(escalation, request);
+    }
+  }
+
+  @Transactional
+  public EscalationRequest save(@NotNull Escalation escalation, @NotNull Request request) {
+    //TODO: add Precondition for escalation null id or name
+    //TODO: add Precondition for request null id or name
+    requestService.save(request);
+    escalationService.save(escalation);
+    EscalationRequest er = new EscalationRequest();
+    er.setEscalationId(escalation.getEscalationId());
+    er.setRequestId(request.getRequestId());
+    return escalationRequestRepository.save(er);
   }
 }
--- src/test/java/com/example/demo/EscalationRequestServiceTest.java
+++ src/test/java/com/example/demo/EscalationRequestServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.example.demo.entity.Escalation;
 import com.example.demo.entity.Request;
 import com.example.demo.service.EscalationRequestService;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
@@ -26,12 +27,55 @@ public class EscalationRequestServiceTest {
   }
 
   @Test
-  void getEscalationRequestsTest() {
+  void getExistingEscalationRequestsTest() {
     Collection<Request> requests = escalationRequestService.getEscalationRequests(111);
     assertTrue(Integer.valueOf(requests.size()).equals(2));
     assertTrue(requests.stream().anyMatch(request -> request.getRequestId().equals(1)));
     assertTrue(requests.stream().anyMatch(request -> request.getName().equals("REQUEST 1")));
     assertTrue(requests.stream().anyMatch(request -> request.getRequestId().equals(11)));
     assertTrue(requests.stream().anyMatch(request -> request.getName().equals("REQUEST 11")));
+  }
+
+  @Test
+  void saveNewEscalationRequestTest() {
+    // TODO: cleanup below
+    Escalation e = new Escalation();
+    e.setEscalationId(555);
+    e.setName("REQUEST 555");
+    Request r = new Request();
+    r.setRequestId(5);
+    r.setName("REQUEST 5");
+    escalationRequestService.save(e, r);
+    assertTrue(escalationRequestService.getEscalations(e.getEscalationId()).get().stream().anyMatch(escalation -> escalation.getEscalationId().equals(555)));
+    assertFalse(escalationRequestService.getEscalations(e.getEscalationId()).get().stream().anyMatch(escalation -> escalation.getEscalationId().equals(666)));
+  }
+
+  @Test
+  void saveEscalationWithMultipleRequestsTest() {
+    // TODO: cleanup below
+    Escalation e = new Escalation();
+    e.setEscalationId(777);
+    e.setName("ESCALATION 777");
+
+    Request r1 = new Request();
+    r1.setRequestId(7);
+    r1.setName("REQUEST 7");
+
+    Request r2 = new Request();
+    r2.setRequestId(77);
+    r2.setName("REQUEST 77");
+
+    Collection<Request> requests = new ArrayList<>();
+    requests.add(r1);
+    requests.add(r2);
+    escalationRequestService.save(e, requests);
+
+    assertTrue(escalationRequestService.getEscalationRequests(e.getEscalationId()).stream().anyMatch(request -> request.getRequestId().equals(7)));
+    assertTrue(escalationRequestService.getEscalationRequests(e.getEscalationId()).stream().anyMatch(request -> request.getRequestId().equals(77)));
+  }
+
+  @Test
+  void checkNotSavePreexistingEscalationRequest() {
+//    assertTrue(false);
   }
 }
